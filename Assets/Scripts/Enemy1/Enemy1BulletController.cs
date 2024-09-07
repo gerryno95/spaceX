3f9c18b6a141be90db8b39f7995d0a04483a78ca
gerryno95 <80595508+gerryno95@users.noreply.github.com>
2024-09-07 22:14:49 +0200

    Enemy1 engine

--- Assets/Scripts/Enemy1/Enemy1BulletController.cs
+++ Assets/Scripts/Enemy1/Enemy1BulletController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Enemy1BulletController : MonoBehaviour
 {
+    [SerializeField] float lifeTime = 7f;
     [SerializeField] float vel = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 20f);
+        Destroy(gameObject, lifeTime);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/SpawnData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnData", order = 1)]
+public class SpawnData : ScriptableObject
+{
+
+    [Serializable]
+    public class SpawnObject
+    {
+        public GameObject prefab;
+        public float minSize = 1f;
+        public float maxSize = 1f;
+    }
+
+    [SerializeField] SpawnObject[] objects;
+
+
+    public SpawnObject[] GetSpawnObjects()
+    {
+        return this.objects;
+    }
+}
